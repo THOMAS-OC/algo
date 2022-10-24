14facedb6ee805f664db88d91b2e6daba8f6fc6f
ESTIVAL Thomas <thomasestivaloc@gmail.com>
2022-10-24 14:06:16 +0200

    doc du bubble sort

--- 5_buble_sort.py
+++ 5_buble_sort.py
@@ -2,7 +2,7 @@ i = 0
 
 nb_boucle = 0
 
-array = [3,5,2]
+array = [3,5,2, -7, -9]
 
 tested = False
 
@@ -17,21 +17,24 @@ while True:
             array[i] = array[i+1]
             
             array[i+1] = temp
-            
-            temp = 0
-            
+
+            # Si un test a été relevé alors la liste n'était pas triée !
             tested = True
             
         i += 1
         
+    # La liste vient d'être parcour car array[i+1] renvoie une erreur.
     except :
         
+        # Si aucun test relevé alors la liste est triée !
         if not tested :
             break
         
+        # La liste a été parcouru, on reset tested
         else :
             tested = False
             
+        # on reset i dans tous les cas pour reparcourir la liste
         i = 0
 
 print(array)
